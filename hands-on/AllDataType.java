bcc9834e6d319e167f7508bcbddf796297a9120d
amansingh0103 <105193001+amansingh0103@users.noreply.github.com>
2022-05-11 11:02:03 +0530

    Add files via upload

---
+++ hands-on/AllDataType.java
@@ -0,0 +1,42 @@
+package corejava;
+
+public class AllDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myNum1, myNum2,myNum3,myNum4,myNum5,myNum6,myNum7,myNum8,myNum9,myNum10,result1,result2,result3;
+		float myFloat1,myFloat2,myFloat3,myFloat4 ,result4,result5;
+		myNum1 = 500;
+		myNum2 = 600;
+		myNum3 = 100;
+		myNum4 = 200;
+		myNum5 = 300;
+		myNum6 = 400;
+		myFloat1 = 500f;
+		myFloat2 = 600f;
+		myFloat3 = 700f;
+		myFloat4 = 80f;
+		result1 = myNum1 + myNum2;
+		result2= myNum3 - myNum4;
+		result3= myNum5 * myNum6;
+		result4= myFloat1 / myFloat2;
+		result5= myFloat3 % myFloat4;
+		System.out.println("Number 1: " +myNum1);
+		System.out.println("Number 2: " +myNum2);
+		System.out.println("Number 3: " +myNum3);
+		System.out.println("Number 4: " +myNum4);
+		System.out.println("Number 5: " +myNum5);
+		System.out.println("Number 6: " +myNum6);
+		System.out.println("Number 7: " +myFloat1);
+		System.out.println("Number 8: " +myFloat2);
+		System.out.println("Number 9: " +myFloat3);
+		System.out.println("Number 10: " +myFloat4);
+		System.out.println("Number 1 + Number 2: " +result1);
+		System.out.println("Number 3 - Number 4: " +result2);
+		System.out.println("Number 5 * Number 6: " +result3);
+		System.out.println("Number 7 / Number 8: " +result4);
+		System.out.println("Number 9 % Number 10: "+result5);
+		}
+		}
+
+
