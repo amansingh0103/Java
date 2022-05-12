8b737d6f826d975f85abdfff8db45f06474150e5
amansingh0103 <105193001+amansingh0103@users.noreply.github.com>
2022-05-12 12:57:41 +0530

    Add files via upload

---
+++ hands-on/Implicitandexplicit.java
@@ -0,0 +1,19 @@
+package corejava;
+
+public class Implicitandexplicit {
+
+	public static void main(String[] args) {
+		int i = 500;
+		
+		//automatic type conversion
+        
+		double d= i;
+		
+		System.out.println("Int value "+i);
+		System.out.println("double value "+d);
+		
+		
+        
+	}
+
+}
