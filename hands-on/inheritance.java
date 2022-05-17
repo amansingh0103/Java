162f51da37bddc854cd4860636307134e0a853fa
amansingh0103 <105193001+amansingh0103@users.noreply.github.com>
2022-05-17 15:46:59 +0530

    Add files via upload

---
+++ hands-on/inheritance.java
@@ -0,0 +1,25 @@
+package corejava;
+
+
+		public class inheritance {
+			public void print() {
+				System.out.println("Employee");
+			}
+			}
+			class Department extends inheritance{
+				public void show() {
+				System.out.println("NMMC");
+				}
+				}
+			class RunMethod{
+			    
+			    public static void main(String[] args) {        
+			    	Department s= new Department();
+			        s.print();
+			        s.show();
+			        
+
+	}
+
+			}
+	
