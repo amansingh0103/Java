65fd6ffaa4fdaff43fb43e7e5b48e500a728d136
amansingh0103 <105193001+amansingh0103@users.noreply.github.com>
2022-05-17 14:08:30 +0530

    Add files via upload

---
+++ practical/CharDataType.java
@@ -0,0 +1,23 @@
+package corejava;
+
+	public class CharDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// declare char datatype variable
+		char name,marks,grade;
+		// initialize value to variable
+	name='A';
+	marks ='8';
+	grade = 'B';
+		//printing on console
+	System.out.println("Name:"+name);
+	System.out.println("Marks:"+marks);
+	System.out.println("Grade:"+grade);
+		}
+	}
+
+		
+		
+		
+		
