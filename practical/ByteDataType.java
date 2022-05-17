f746cef01f06a69e77c0b3bfb410d8fdf74cafdf
amansingh0103 <105193001+amansingh0103@users.noreply.github.com>
2022-05-17 14:11:05 +0530

    Add files via upload

---
+++ practical/ByteDataType.java
@@ -0,0 +1,16 @@
+package corejava;
+
+public class ByteDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte myByte1,myByte2;   
+		myByte1 = 127;
+		myByte2 = -48;
+		System.out.println("Byte 1: " +myByte1);
+		System.out.println("Byte 2: " +myByte2);
+	
+
+	}
+
+}
