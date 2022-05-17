1de23bfaafa888f900248d9214a8f4463fef79f3
amansingh0103 <105193001+amansingh0103@users.noreply.github.com>
2022-05-17 14:12:56 +0530

    Add files via upload

---
+++ practical/BooleanDataType.java
@@ -0,0 +1,11 @@
+package corejava;
+
+public class BooleanDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean myBool=true;
+		if(myBool==true)
+		System.out.println("I am useing a Boolean data type:"+myBool);
+	}
+}
