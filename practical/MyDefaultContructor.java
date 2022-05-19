ca2cb89112eb76f2e7723670eeb36a606e1a0795
amansingh0103 <105193001+amansingh0103@users.noreply.github.com>
2022-05-19 11:50:57 +0530

    Add files via upload

---
+++ practical/MyDefaultContructor.java
@@ -0,0 +1,12 @@
+package corejava;
+
+public class MyDefaultContructor {
+	MyDefaultContructor(){
+		System.out.println("This is Defult Contructor/No Arugment contructor"
+);	}
+	public static void main(String[] args) {
+		MyDefaultContructor mdc=new MyDefaultContructor();
+
+	}
+
+}
