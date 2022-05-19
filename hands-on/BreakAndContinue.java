ffdf5adb104ce9ef2ae7e869027271a4bb615139
amansingh0103 <105193001+amansingh0103@users.noreply.github.com>
2022-05-19 13:24:07 +0530

    Add files via upload

---
+++ hands-on/BreakAndContinue.java
@@ -0,0 +1,28 @@
+package corejava;
+
+public class BreakAndContinue {
+
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++) {
+			if(i==4) {
+				break;
+			}
+			System.out.println(i);
+		}
+		
+			for(int i=1;i<10;i++) {
+				if(i==8) {
+					continue;
+				}
+				System.out.println(i);
+			}
+
+		}
+	
+		
+		
+	}
+	
+
+
+
