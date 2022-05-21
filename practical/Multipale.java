e9d9a81ae08c3eda838be5c40c3aeda53c45167f
amansingh0103 <105193001+amansingh0103@users.noreply.github.com>
2022-05-21 16:46:24 +0530

    Add files via upload

---
+++ practical/Multipale.java
@@ -0,0 +1,18 @@
+package corejava;
+
+public class Multipale {
+
+	public static void main(String[] args) {
+	int a[][]= {{2,5,9},{5,8,6},{1,3,4}};
+	
+	//prining array
+	for (int i=0;i<3;i++) {
+	for	(int j=0;j<3;j++)
+	System.out.println(a[i][j]+"");
+	System.out.println();
+	
+	}
+
+	}
+
+}
