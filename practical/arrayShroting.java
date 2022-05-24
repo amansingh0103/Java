7f928a083620ee81694b1aa974a94fe4e8041734
amansingh0103 <105193001+amansingh0103@users.noreply.github.com>
2022-05-24 11:42:38 +0530

    Add files via upload

---
+++ practical/arrayShroting.java
@@ -0,0 +1,35 @@
+package corejava;
+//Main driver method
+
+import java.util.Arrays;
+
+public class arrayShroting {
+
+	public static void main(String[] args) {
+		//Java Program to Sort Array of Integers
+		//by Default Sorts in an Ascending Order
+		//using Arrays.sort() Method
+
+		 
+
+		//Importing Arrays class from the utility class
+		//Main class                                            
+		    {
+		        // Custom input array
+		        int[] arr = { 10, 20, 30, 40, 50, 45, 35, 25 }; // SD Array
+
+		 
+
+		        // Applying sort() method over to above array
+		        // by passing the array as an argument
+		        Arrays.sort(arr);
+
+		 
+
+		        // Printing the array after sorting
+		     // System.out.printf("Modified arr[] : %s", Arrays.toString(arr));
+		      System.out.println("Modified arr[] :"+ Arrays.toString(arr));
+		    }
+	}
+}
+		
